bbc42f9f782efda3f3340465a6a268a7cec2fe18
Dave Kennedy <david@bangline.co.uk>
2011-11-17 14:24:27 +0000

    some more work on deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,24 @@
-require 'json'
-
-user = JSON.parse('../dna.json')
-
 ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
-set :application, "capistrano_demo"
-set :user, user['name']
-set :password, user['password']
-set :deploy_to, "/home/#{user}/app"
+require 'json'
+set :user_details, JSON.parse(IO.read('dna.json'))
 
-set :repository, "git@github.com:bangline/icapistrano_demo.git"
+set :domain, "capdemo.bangline.co.uk"
+
+set :application, domain
+set :user, user_details['name']
+set :password, user_details['password']
+set :deploy_to, "/home/#{user}"
+set :use_sudo, false
+
+set :repository, "git@github.com:bangline/capdemo.git"
 set :scm, :git
 set :branch, "master"
 
-role :app, "capdemo.bangline.co.uk"
-role :web, "capdemo.bangline.co.uk"
-role :db, "capdemo.bangline.co.uk", :primary => true
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
