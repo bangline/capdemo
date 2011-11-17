a5b42accfb804db70b658373fec97a39f6592604
Dave Kennedy <david@bangline.co.uk>
2011-11-17 02:51:56 +0000

    the app is alive

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+ssh_options[:forward_agent] = true
+
+set :application, "capistrano_demo"
+set :user, "capdemo"
+set :deploy_to, "/home/#{user}/app"
+
+set :repository, "git@github.com:bangline/icapistrano_demo.git"
+set :scm, :git
+set :branch, "master"
+
+role :app, "capdemo.bangline.co.uk"
+role :web, "capdemo.bangline.co.uk"
+role :db, "capdemo.bangline.co.uk", :primary => true
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoDemo::Application.config.secret_token = '06c63a662d4f99ba05f77cd454264c505685c9d62b505908edd76d8ce31c3f80e239dc6e091b48b27f85ff162ab1e7a8be27aeb134aa614c43fca6dd7feeb0d0'
