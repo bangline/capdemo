dd264b0f29b6a6678c76c81a4b4ec22b82cf1cbe
Dave Kennedy <david@bangline.co.uk>
2011-11-17 23:14:20 +0000

    update for rvm in deploy and rvmrc

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :user, user_details['name']
 set :password, user_details['password']
 set :deploy_to, "/home/#{user}"
 set :use_sudo, false
+set :gemset, "capdemo"
 
 set :repository, "git@github.com:bangline/capdemo.git"
 set :scm, :git
@@ -19,11 +20,28 @@ role :app, domain
 role :web, domain
 role :db, domain, :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+require "bundler/capistrano"
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) 
+require "rvm/capistrano"           
+set :rvm_ruby_string, "1.9.2@#{gemset}" 
+
+namespace :deploy do
+  task :start do ; end
+
+  task :stop do ; end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+
+  task :create_gemset do
+    run "rvm use 1.9.2@#{gemset} --create"
+  end
+end
+
+after "deploy", "rvm:trust_rvmrc"
